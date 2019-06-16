9990e80190806ee37081465ca1e912b3f64045b0
Jonas Staudenmeir <mail@jonas-staudenmeir.de>
2019-06-16 17:08:45 +0200

    Remove unused variables

--- tests/Concerns/AutoloadsRelationshipsTest.php
+++ tests/Concerns/AutoloadsRelationshipsTest.php
@@ -26,7 +26,7 @@ class AutoloadsRelationshipsTest extends TestCase
     public function testGetRelationshipFromMethodAfterDisableAutoLoadCalledDoesntAutoLoad()
     {
         $models = TraitModel::all();
-        $related = $models[0]->disableAutoload()->myRelationship;
+        $models[0]->disableAutoload()->myRelationship;
 
         $this->assertFalse($models[1]->relationLoaded('myRelationship'));
     }
@@ -35,7 +35,7 @@ class AutoloadsRelationshipsTest extends TestCase
     {
         $models = TraitModel::all();
         $models[0]->setAutoloadThreshold(2);
-        $related = $models[0]->myRelationship;
+        $models[0]->myRelationship;
 
         $this->assertFalse($models[1]->relationLoaded('myRelationship'));
     }
@@ -44,7 +44,7 @@ class AutoloadsRelationshipsTest extends TestCase
     public function testGetRelationshipFromMethodUnderThresholdDoesAutoLoad()
     {
         $models = TraitModel::all();
-        $related = $models[0]->myRelationship;
+        $models[0]->myRelationship;
 
         $this->assertTrue($models[1]->relationLoaded('myRelationship'));
     }
@@ -64,7 +64,7 @@ class AutoloadsRelationshipsTest extends TestCase
         $models = TraitModel::all();
         $models[0]->setLogging('jitLogger', $driver);
 
-        $related = $models[0]->myRelationship;
+        $models[0]->myRelationship;
 
         $this->assertTrue($models[1]->relationLoaded('myRelationship'));
     }
--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -16,7 +16,7 @@ class ModelTest extends TestCase
     public function testGetRelationshipFromMethodAfterDisableAutoLoadCalledDoesntAutoLoad()
     {
         $models = DummyModel::all();
-        $related = $models[0]->disableAutoload()->myRelationship;
+        $models[0]->disableAutoload()->myRelationship;
 
         $this->assertFalse($models[1]->relationLoaded('myRelationship'));
     }
@@ -25,7 +25,7 @@ class ModelTest extends TestCase
     {
         $models = DummyModel::all();
         $models[0]->setAutoloadThreshold(2);
-        $related = $models[0]->myRelationship;
+        $models[0]->myRelationship;
 
         $this->assertFalse($models[1]->relationLoaded('myRelationship'));
     }
@@ -34,7 +34,7 @@ class ModelTest extends TestCase
     public function testGetRelationshipFromMethodUnderThresholdDoesAutoLoad()
     {
         $models = DummyModel::all();
-        $related = $models[0]->myRelationship;
+        $models[0]->myRelationship;
 
         $this->assertTrue($models[1]->relationLoaded('myRelationship'));
     }
